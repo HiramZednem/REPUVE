43cfaefa5d95c8a722123f378a719292cfd1ef99
EczarEsco <eczar22029@gmail.com>
2022-11-23 14:32:02 -0600

    Created worker register page and principal footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Footer from "./components/Footer.jsx";
+import './assets/styles/Footer.css'
+import Formulario from "./components/Formulario.jsx";
+import WorkerRegistrer from "./pages/WorkerRegistrer.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WorkerRegistrer/>
   </React.StrictMode>
 )
